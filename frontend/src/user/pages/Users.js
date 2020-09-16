0e4dfe4a463c0ee511a2f6b6a81f6ca5ec0d6d03
florianbar <florianbar01@gmail.com>
2020-09-16 14:22:39 +0200

    Added UsersList and UserItem components

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return (
-        <h2>Users Works!</h2>
-    );
+    const USERS = [
+        {id: "u1", name: "Flo Bar", image: "dsa", places: 2 },
+        {id: "u2", name: "Candy Bar", image: "dsad", places: 4 }
+    ];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
