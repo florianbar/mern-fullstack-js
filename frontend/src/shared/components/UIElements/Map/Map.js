c95fb880d62f681c2724a1379f2e8afe061d0f11
florianbar <florianbar01@gmail.com>
2020-09-17 09:16:49 +0200

    Replaced google maps with OpenLayers

--- frontend/src/shared/components/UIElements/Map/Map.js
+++ frontend/src/shared/components/UIElements/Map/Map.js
@@ -5,25 +5,30 @@ import './Map.css';
 const Map = props => {
     const mapRef = useRef();
 
-    // const { center, zoom } = props;
+    const { center, zoom } = props;
 
-    // useEffect(() => {
-    //     const map = new window.google.maps.Map(mapRef.current, {
-    //         center: center,
-    //         zoom: zoom
-    //     });
-    
-    //     new window.google.maps.Marker({
-    //         position: center,
-    //         map: map
-    //     });
-    // }, [center, zoom]);
+    useEffect(() => {
+        new window.ol.Map({
+            target: mapRef.current.id,
+            layers: [
+                new window.ol.layer.Tile({
+                source: new window.ol.source.OSM()
+                })
+            ],
+            view: new window.ol.View({
+                center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+                zoom: zoom
+            })
+        });
+    }, [center, zoom]);
 
     return (
         <div 
             ref={mapRef}
             className={`map ${props.className}`} 
-            style={props.style}>The Map</div>
+            style={props.style}
+            id="map"
+        ></div>
     );
 };
 
